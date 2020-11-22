8e5acf3fc6ad96963de64d25936e85554daac5f4
g <gWOLF3@protonmail.com>
2020-11-21 21:44:25 -0600

    fix that

--- move.sh
+++ move.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp mobile/build/outputs/apk/freedom/release/mobile-freedom-release-unsigned.apk ../../aosp/android_system_privapp_Hotspot/hotspot.ap
+cp mobile/build/outputs/apk/freedom/release/mobile-freedom-release-unsigned.apk ../../aosp/android_system_priv-app_Hotspot/hotspot.apk
